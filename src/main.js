b07ef0e2c0ecae482ff58179dba0316f48167104
yan-xz <yanyingsong@outlook.com>
2018-08-05 17:34:45 +0800

    s

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-/*
-{"name": "app","version": "0.0.0","main": "main.js","scripts": {"build": "electron-packager . --win --electron-version=1.7.10 --icon=app.ico","start": "electron.exe ."}}
-*/
 const AppUrl = "https://im.dingtalk.com"
-, electron = require('electron'), width = 1000, height = 600
-, main_window = () => {
-electron.Menu.setApplicationMenu(null)
-let mainWindow = new electron.BrowserWindow({
-width: width, height: height, transparent: true, frame: true,
-})
-mainWindow.on('closed', () => { mainWindow = null }).loadURL(AppUrl)
-new_window(mainWindow)
-}
-, new_window = w => {
-w.webContents.on('new-window', (event, url, fname, disposition, options) => {
-event.preventDefault()
-let childWindow = new electron.BrowserWindow({ width: width, height: 800 })
-childWindow.on('closed', () => { mainWindow = null }).loadURL(url)
-new_window(childWindow)
-})
-}
+    , electron = require('electron'), width = 1000, height = 600
+    , main_window = () => {
+        electron.Menu.setApplicationMenu(null)
+        let mainWindow = new electron.BrowserWindow({
+            width: width, height: height, transparent: true, frame: true,
+        })
+        mainWindow.on('closed', () => { mainWindow = null }).loadURL(AppUrl)
+        new_window(mainWindow)
+    }
+    , new_window = w => {
+        w.webContents.on('new-window', (event, url, fname, disposition, options) => {
+            event.preventDefault()
+            let childWindow = new electron.BrowserWindow({ width: width, height: 800 })
+            childWindow.on('closed', () => { mainWindow = null }).loadURL(url)
+            new_window(childWindow)
+        })
+    }
 electron.app.on("ready", main_window).on('window-all-closed', () => {
-electron.app.quit()
+    electron.app.quit()
 });
