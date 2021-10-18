21edb94aa03ecf8862883a544fd41a5ac4b90e3d
マツウィン（松文） <yanyingsong@outlook.com>
2021-10-18 14:30:57 +0800

    Update simple.go

--- examples/simple.go
+++ examples/simple.go
@@ -7,21 +7,21 @@ import (
 )
 
 func main() {
-	fmt.Println("program running.")
 
+	// 添加异步任务 work_1，后台持续运行。
 	siggroup.Async(func() {
 		fmt.Println("work_1 ...")
-		for {
-		}
+		for {}
 	})
 
+	// 添加异步任务 work_2，短暂运行后退出。
 	siggroup.Async(func() {
 		fmt.Println("work_2 ...")
 		time.Sleep(time.Second)
 	})
 
-	// wait.
+	// 等待任务结束，注意！只要有一个任务退出就退出所有。
 	siggroup.Wait(func() {
-		fmt.Println("program shutdown.")
+		fmt.Println(":shutdown")
 	})
 }
