964d971f8d8400ab7d61321e0fba167ddb949266
マツウィン（松文） <yanyingsong@outlook.com>
2021-11-08 20:10:12 +0800

    Update errcause_test.go

--- x/errcause/errcause_test.go
+++ x/errcause/errcause_test.go
@@ -1,28 +1,25 @@
 package errcause_test
 
 import (
-	"github.com/matsuwin/siggroup/x/errcause"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"testing"
+
+	"github.com/matsuwin/siggroup/x/errcause"
 )
 
-func mkError() error {
-	_, err := ioutil.ReadFile("xxx.txt")
-	return errors.New(err.Error())
-}
-
 func Test(t *testing.T) {
+	defer errcause.Recover()
 
-	// 错误恢复 recover call errcause.Keep
-	defer func() {
-		if ei := recover(); ei != nil {
-			errcause.Keep(ei)
-		}
-	}()
-
-	// 模拟一个错误抛出调用
 	if err := mkError(); err != nil {
 		panic(err)
 	}
+}
+
+func mkError() (_ error) {
+	_, err := ioutil.ReadFile("xxx.txt")
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	return
 }
